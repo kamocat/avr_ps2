b7a2eda1759f31db5a788c16feb8f2b1324cf8ec
Marshal Horn <kamocat@gmail.com>
2014-06-26 15:05:11 -0700

    Created macros for telling elapsed time.
    These are useful both for a simple scheduler, and for enforcing clock rate requirements.
    In other words, if we only get part of a message, we will notice that it's taking too long
    and won't get hung up.

---
+++ tdelta.h
@@ -0,0 +1,16 @@
+/* This header is for measuring delta time since last transaction.
+ * Since it may be used in interrupt scope, it is defined as macros.
+ * The setup function, however, is a regular function.
+ */
+
+uint8_t setup_delta_timer( void ) {
+	TCCR3B = 1;
+	/* This give 1us resolution, and a 64ms max count.
+	 * If we change the prescaler to 2, then we have a 8us resolution
+	 * and a 512ms max count.
+	 */
+}
+
+#define CHECK_TDELTA( tprev ) ( TCNT3 - tprev )
+
+#define RESET_TDELTA( tprev ) tprev = TCNT3
